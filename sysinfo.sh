9e3a1ec761098b1ce6c539285fbda9472041df3a
JAMAL-q <kam4damczyk@gmail.com>
2025-11-21 18:57:29 +0100

    Update sysinfo.sh

--- sysinfo.sh
+++ sysinfo.sh
@@ -4,27 +4,49 @@
         cpu=$(lscpu | grep ^"Nazwa modelu:")
         echo "CPU: $cpu"
     }
-    # get_ram(){
-
-    # }
+    get_ram(){
+        mem1=$(free -m | awk '/^Pamięć:/ {print $2}');
+        mem2=$(free -m | awk '/^Pamięć:/ {print $3}');
+        mem=$(100 * $mem1 / $mem2);
+        echo "$mem1/$mem2 MiB (mem% used)";
+    }
 
     get_load(){
         load=$(uptime | awk -F'load average: ' '{print $2}')
-        LOAD: $load
+        echo "LOAD: $load"
     }
     get_uptime(){
         time=$()
     }
-    # get_kernel
-    # get_gpu
+    get_kernel(){
+        karnel=$(uname -r)
+        echo "KARNEL: $karnel"
+    }
+    get_gpu(){
+        gpu=$(lspci | grep -i 'vga\|3d' | awk -F ': ' '{print $2}')
+        echo " GPU: $gpu"
+    }
     get_user(){
         user=$(uname)
         echo "USER: $user"
     }
     
-    # get_shell
-    # get_processes
-    # get_threads
-    # get_ip
+    #get_shell
+
+    get_processes(){
+        oc=$(ps -e --no-headers |wc -l)
+        echo "prosesses: $proc"
+    }
+    get_threads(){
+        thre=$(ps -eLf --no-headers |wc -l)
+        echo "threads $thre"
+    }
+    get_ip(){
+        ip=$(ip addr | grep 'inet[[:space:]]192' | awk -F ' ' '{print $2, $4}')
+        echo "IP: $ip"
+    }
     # get_dns
-    # get_internet
+
+    # get_internet 
+    
+    #    ping -c 8.8.8.8
